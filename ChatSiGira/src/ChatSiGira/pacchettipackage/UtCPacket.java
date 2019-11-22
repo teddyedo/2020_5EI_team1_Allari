730c1a8219a5ad6fa3b6386b2ae38c26cc6b6162
Edoardo <35118807+teddyedo@users.noreply.github.com>
2019-11-22 12:00:32 +0100

    Interpreter branch (#9)
    * Created PacketClass
    
    * Created PacketClass
    
    * Modify branch (#5)
    
    * Started GUI of main interface
    
    * We have created the MCV structure.
    
    * Started Login interface
    
    * deleted some files
    
    * Structure ridefinition

---
+++ ChatSiGira/src/ChatSiGira/pacchettipackage/UtCPacket.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pacchettipackage;
+
+/**
+ *
+ * @author Zerbato,Nicolis
+ */
+public class UtCPacket {
+    private byte[] ID;
+    private String Message;
+
+    public UtCPacket(byte[] ID, String Message) {
+        this.ID = ID;
+        this.Message = Message;
+    }
+    
+    /**
+     * 
+     * @return ID
+     */
+
+    public byte[] getID() {
+        return ID;
+    }
+    
+    /**
+     * 
+     * @param ID 
+     */
+
+    public void setID(byte[] ID) {
+        this.ID = ID;
+    }
+    
+    /**
+     * 
+     * @return Message
+     */
+
+    public String getMessage() {
+        return Message;
+    }
+    
+    /**
+     * 
+     * @param Message 
+     */
+
+    public void setMessage(String Message) {
+        this.Message = Message;
+    }
+
+  
+
+   
+    
+}
