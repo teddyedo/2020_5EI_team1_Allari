03789459643232a74f56dd27ddedc8dd1996de9d
Edoardo <edo.allari@gmail.com>
2019-11-28 22:00:21 +0100

    started connection to server

---
+++ ChatSiGira/src/ChatSiGira/Connection.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ChatSiGira;
+
+/**
+ *
+ * @author Allari Edoardo
+ * 
+ * 
+ */
+
+public class Connection {
+    
+    public static void main(String[] args) {
+        
+    }
+}
