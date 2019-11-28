bb397cabc32a63a172ba99487685026e7c2b8706
Edoardo <edo.allari@gmail.com>
2019-11-28 21:15:56 +0100

    concluded packet interpreter

--- ChatSiGira/src/ChatSiGira/interpreter/Interpreter.java
+++ ChatSiGira/src/ChatSiGira/interpreter/Interpreter.java
@@ -8,6 +8,8 @@ package ChatSiGira.interpreter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import pacchettipackage.*;
+import com.google.gson.*;
+import ChatSiGira.UserList;
 
 /**
  *
@@ -18,15 +20,20 @@ import pacchettipackage.*;
  */
 
 public class Interpreter {
+    
+    ArrayList<Object> packetList = new ArrayList<>();
 
-    public byte[] interpret(byte[] data) {
+    public Object interpret(byte[] data) {
         byte opCode = data[0];
 
         switch (opCode) {
 
             case 01:                        //message user to user
                 return utuDPacketToBytes(data);
-
+         
+            case 05:
+                return utcDPacketToBytes(data);
+                
             case 11:                        //disconnection
                 return disconnectionServerPacketToBytes(data);
 
@@ -34,12 +41,13 @@ public class Interpreter {
                 return createRegistrationAckFromBytes(data);
 
             case 51:                        //group user list 
-                return list(data);
+                return groupUsersListPacketToBytes(data);
                 
-            case 255:
+            case (byte) 255:
                 return errorsPacketToBytes(data);
 
         }
+        return null;
 
     }
 
@@ -105,12 +113,96 @@ public class Interpreter {
         
         GroupUsersListPacket groupUsersListPacket = new GroupUsersListPacket(null, null, null);
         
+        byte type = data[1];
+        
+        byte[] jsonByte = Arrays.copyOfRange(data, 3, data.length - 1);
+        
+        String jsonString = new String(jsonByte);
+        
+        ArrayList<String> userList = new ArrayList<>();
+        
+        Gson json = new Gson();
+        userList = json.fromJson(jsonString, userList.getClass());
+        
+        switch(type){
+            
+            case 0:
+                UserList.chatUserList.clear();
+                UserList.chatUserList = userList;
+ 
+            case 1:
+                UserList.chatUserList.add(userList.get(0));
+                
+            case 2:
+                UserList.chatUserList.remove(userList.get(0));
+        }
+        
+
+        
+        
+        
+        
         return groupUsersListPacket;
     }
 
     private UtuDPacket utuDPacketToBytes(byte[] data){
         
+        if(data.length > 2048)
+            throw new UnsupportedOperationException();
+        
         UtuDPacket utuDPacket = new UtuDPacket(null, null);
+        
+        ArrayList<byte[]> variableElements = new ArrayList<>();
+
+        int start = 1;
+        
+        for(int i = 1; i < data.length; i++){
+            if(data[i] == 0){
+                variableElements.add( Arrays.copyOfRange( data, start, i));
+                start = i + 1;
+            }
+        }
+        
+        //message
+        variableElements.add( Arrays.copyOfRange( data, start, data.length -1));
+       
+        
+        String sourceAlias = new String(variableElements.get(0));
+        String message = new String(variableElements.get(1));
+        
+        utuDPacket.setMessage(message);
+        utuDPacket.setSourceAlias(sourceAlias);
+        
+        boolean added = false;
+        
+        for (Object p : packetList){
+            if(p.getClass().isInstance(utuDPacket)){
+                UtuDPacket u = (UtuDPacket) p;
+                if(u.getSourceAlias() == utuDPacket.getSourceAlias()){
+                    u.setMessage(u.getMessage() + message);
+                    added = true;
+                    if(data.length < 2048){
+                        packetList.remove(u);
+                        return u;          
+                    }
+                }
+            }        
+        }    
+        
+        if(data.length == 2048 && added == false)
+              packetList.add(utuDPacket);
+        
+        else if (data.length < 2048){
+            return utuDPacket;
+        }
+        return null;
+    }
+    
+    
+    //bisogna implementare un nuovo pacchetto per i pacchetti che arrivano dalle chat pubbliche
+    private UtcDPacket utcDPacketToBytes(byte[] data){
+        
+        UtcDPacket utcDPacket = new UtcDPacket(null, null);
         
         ArrayList<byte[]> variableElements = new ArrayList<>();
 
@@ -131,9 +223,33 @@ public class Interpreter {
         String sourceAlias = new String(variableElements.get(0));
         String message = new String(variableElements.get(1));
         
-        utuDPacket.setMessage(message);
-        utuDPacket.setSourceAlias(sourceAlias);
+        utcDPacket.setMessage(message);
+        utcDPacket.setSourceAlias(sourceAlias);
         
-        return utuDPacket;
+        boolean added = false;
+        
+        for (Object p : packetList){
+            if(p.getClass().isInstance(utcDPacket)){
+                UtcDPacket u = (UtcDPacket) p;
+                if(u.getSourceAlias() == utcDPacket.getSourceAlias()){
+                    u.setMessage(u.getMessage() + message);
+                    added = true;
+                    if(data.length < 2048){
+                        packetList.remove(u);
+                        return u;          
+                    }
+                }
+            }        
+        }    
+        
+        if(data.length == 2048 && added == false)
+              packetList.add(utcDPacket);
+        
+        else if (data.length < 2048){
+            return utcDPacket;
+        }
+        return null;
+        
     }
+    
 }
