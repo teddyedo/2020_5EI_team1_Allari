2036ceddebbc9b0404c9418810a08a73e51b6856
Edoardo <edo.allari@gmail.com>
2019-12-08 11:35:03 +0100

    moved GUI into ChatSiGira package

--- ChatSiGira/src/ChatSiGira/graphicinterface/ChatInterface.java
+++ ChatSiGira/src/ChatSiGira/graphicinterface/ChatInterface.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package graphicinterface;
+package ChatSiGira.graphicinterface;
 
 import java.awt.CardLayout;
 
